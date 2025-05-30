43a4a79107432875aa8d756fc93ba11ae9d53a33
Skyf0l <59019720+skyf0l@users.noreply.github.com>
2025-05-30 12:35:36 +0200

    fix: exlint + prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,20 @@
+import prettier from "eslint-config-prettier";
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import js from "@eslint/js";
+import { includeIgnoreFile } from "@eslint/compat";
+import ts from "typescript-eslint";
+import { fileURLToPath } from "node:url";
+
+const gitignorePath = fileURLToPath(new URL("./.gitignore", import.meta.url));
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  // This needs to in a separate object to be a "global ignore". See https://github.com/eslint/eslint/discussions/17429
+  includeIgnoreFile(gitignorePath),
+  js.configs.recommended,
+  ...ts.configs.recommended,
+  // Turns off all rules that are unnecessary or might conflict with Prettier.
+  prettier,
   {
-    ignores: ["**/coverage", "**/.svelte-kit"],
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,ts}"],
     languageOptions: {
       globals: globals.browser,
     },
